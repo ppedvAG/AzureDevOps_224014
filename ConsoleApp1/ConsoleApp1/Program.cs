b351afad3973dba0127f10bf0b31ddbd8f703499
AndreRRR <ruland@outlook.de>
2023-03-02 17:37:38 +0100

    Files aus dem Azure DevOpsRepo

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+
+var PWD = "SehrGeheimePasswort"; //Secrets im Code sind doof
+Console.WriteLine($"Password aus Code == DOOF: {PWD}");
+
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                                       .AddUserSecrets(Assembly.GetExecutingAssembly())
+                                       .Build();
+
+var sec = config.GetSection("Password");
+Console.WriteLine($"Password aus appsettings: {sec.Value}");
+
+
+Console.WriteLine("Ende");
+Console.ReadKey();
